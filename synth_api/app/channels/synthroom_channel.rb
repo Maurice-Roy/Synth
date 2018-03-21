dba61512cfb1720bd1f940f51a28a180a6da31b0
github username <github email address>
2018-03-21 13:25:02 -0400

    mvp commit

--- synth_api/app/channels/synthroom_channel.rb
+++ synth_api/app/channels/synthroom_channel.rb
@@ -2,9 +2,11 @@ class SynthroomChannel < ApplicationCable::Channel
   def subscribed
     synthroom = Synthroom.find(params[:synthroom_id])
     stream_for synthroom
+    # create synth architecture for this user here
   end
 
   def unsubscribed
+    #destroy unused synth architecture for this user here
     # Any cleanup needed when channel is unsubscribed
   end
 end
